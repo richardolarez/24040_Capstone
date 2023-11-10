a0e28dc950df2a89051225a183b3c56c37acac8a
Richardo Larez <richardolarez@arizona.edu>
2023-11-09 17:16:56 -0700

    New xml parser

--- UofA - SW_EE/BasicVisioDrawing_files/xml_parser.py
+++ UofA - SW_EE/BasicVisioDrawing_files/xml_parser.py
@@ -1,44 +1,46 @@
 import xml.etree.ElementTree as ET
 
 # Load the XML file
-tree = ET.parse('data.xml')
+tree = ET.parse('UofA - SW_EE/BasicVisioDrawing_files/data.xml')
 root = tree.getroot()
 
 # Iterate through the shapes
-for shape in root.findall(".//Shape"):
-    shape_id = shape.get("ID")
-    unique_id = shape.get("UniqueID")
-    name = shape.get("Name")
-    name_u = shape.get("NameU")
+with open('visioObjects.txt', 'w') as f:
+    for shape in root.findall(".//Shape"):
+        shape_id = shape.get("ID")
+        unique_id = shape.get("UniqueID")
+        name = shape.get("Name")
+        name_u = shape.get("NameU")
 
-    # Extract text within the Text element if it exists
-    text_element = shape.find(".//Text")
-    if text_element is not None:
-        text = text_element.text
-    else:
-        text = None
+        # Extract text within the Text element if it exists
+        text_element = shape.find(".//Text")
+        if text_element is not None:
+            text = text_element.text
+        else:
+            text = None
 
-    # Extract X and Y coordinates from the XForm element
-    x_form = shape.find(".//XForm")
-    if x_form is not None:
-        pin_x = x_form.find(".//PinX").text
-        pin_y = x_form.find(".//PinY").text
-        pin_x_unit = x_form.find(".//PinX").get("Unit")
-        pin_y_unit = x_form.find(".//PinY").get("Unit")
-    else:
-        pin_x = None
-        pin_y = None
-        pin_x_unit = None
-        pin_y_unit = None
+        # Extract X and Y coordinates from the XForm element
+        x_form = shape.find(".//XForm")
+        if x_form is not None:
+            pin_x = x_form.find(".//PinX").text
+            pin_y = x_form.find(".//PinY").text
+            pin_x_unit = x_form.find(".//PinX").get("Unit")
+            pin_y_unit = x_form.find(".//PinY").get("Unit")
+        else:
+            pin_x = None
+            pin_y = None
+            pin_x_unit = None
+            pin_y_unit = None
 
-    # Print or process the extracted information as needed
-    print(f"Shape ID: {shape_id}")
-    print(f"Unique ID: {unique_id}")
-    print(f"Name: {name}")
-    print(f"NameU: {name_u}")
-    if text is not None:
-        print(f"Text: {text}")
-    if pin_x is not None and pin_y is not None:
-        print(f"PinX: {pin_x} {pin_x_unit}")
-        print(f"PinY: {pin_y} {pin_y_unit}")
-    print("\n")
+        # Print or process the extracted information as needed
+       
+        f.write(f"Shape ID: {shape_id}\n")
+        f.write(f"Unique ID: {unique_id}\n")
+        f.write(f"Name: {name}\n")
+        f.write(f"NameU: {name_u}\n")
+        if text is not None:
+            f.write(f"Text: {text}\n")
+        if pin_x is not None and pin_y is not None:
+            f.write(f"PinX: {pin_x} {pin_x_unit}\n")
+            f.write(f"PinY: {pin_y} {pin_y_unit}\n")
+        f.write("\n")
---
+++ parser_v1.py
@@ -0,0 +1,38 @@
+import xml.etree.ElementTree as ET
+
+# Parse the XML file
+tree = ET.parse('UofA - SW_EE/BasicVisioDrawing_files/data.xml')
+root = tree.getroot()
+
+# Create a list to hold the objects
+objects = []
+
+# Loop through the shapes
+for shape in root.findall('.//Shape'):
+    # Get the ID, UniqueID, Name, and Text attributes
+    print(shape)
+    shape_id = shape.get('ID')
+    unique_id = shape.get('UniqueID')
+    name = shape.get('Name')
+    text = shape.find('Text').text
+    
+    # Get the PinX and PinY attributes from the XForm element
+    xform = shape.find('XForm')
+    pinx = xform.find('PinX').text
+    piny = xform.find('PinY').text
+    
+    # Create an object with the information
+    obj = {
+        'shape_id': shape_id,
+        'unique_id': unique_id,
+        'name': name,
+        'text': text,
+        'pinx': pinx,
+        'piny': piny
+    }
+    
+    # Add the object to the list
+    objects.append(obj)
+
+# Print the list of objects
+print(objects)
